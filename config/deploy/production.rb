4895ab6d5801fbf34cd93e2cb4423284ef229766
arfn <vynhart@gmail.com>
2019-12-13 23:44:54 +0700

    set capistrano config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,7 @@
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
-
+server "139.59.100.22", user: "vyn", roles: %w{app}
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
